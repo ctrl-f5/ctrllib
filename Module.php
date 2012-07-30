4793e5dcae073d49f524ee2ec9a23250f985b002
CtrlF5 <mythix.dev@gmail.com>
2012-07-30 02:12:47 +0200

    module initialiser moved

--- Module.php
+++ Module.php
@@ -1,38 +1,3 @@
 <?php
 
-namespace Ctrl;
-
-use Zend\Mvc\ModuleRouteListener;
-
-class Module
-{
-    public function onBootstrap($e)
-    {
-        $application = $e->getApplication();
-        /** @var $serviceManager \Zend\ServiceManager\ServiceManager */
-        $serviceManager = $application->getServiceManager();
-
-        // Add initializer to Controller Service Manager that check if controllers needs entity manager injection
-        $serviceManager->addInitializer(function ($instance) use ($serviceManager) {
-            if (method_exists($instance, 'setEntityManager')) {
-                $instance->setEntityManager($serviceManager->get('doctrine.entitymanager.orm_default'));
-            }
-        });
-    }
-
-    public function getConfig()
-    {
-        return include __DIR__ . '/config/module.config.php';
-    }
-
-    public function getAutoloaderConfig()
-    {
-        return array(
-            'Zend\Loader\StandardAutoloader' => array(
-                'namespaces' => array(
-                    __NAMESPACE__ => __DIR__ . '/src/' . __NAMESPACE__,
-                ),
-            ),
-        );
-    }
-}
+require_once __DIR__ . '/src/Ctrl/Module.php';

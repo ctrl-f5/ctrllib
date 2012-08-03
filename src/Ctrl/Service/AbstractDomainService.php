e7d6630fb5bdc5f4f142a880328f0bbce905cfb1
CtrlF5 <mythix.dev@gmail.com>
2012-08-03 18:40:54 +0200

    added collection ordering helper

--- src/Ctrl/Service/AbstractDomainService.php
+++ src/Ctrl/Service/AbstractDomainService.php
@@ -58,4 +58,38 @@ abstract class AbstractDomainService implements
         $serviceName .= 'DomainService';
         return $manager->get($serviceName);
     }
+
+    public function switchOrderInCollection($collection, $id, $dir, $methods = array('getOrder', 'setOrder'))
+    {
+        if (count($methods) == 2) {
+            //prepare for war
+            $get = reset($methods);
+            $set = end($methods);
+            $getBy = function ($collection, $method, $value) {
+                foreach ($collection as $model) {
+                    if (method_exists($model, $method)) {
+                        if ($model->$method() == $value) return $model;
+                    }
+                }
+                return false;
+            };
+
+            //get order of source model and check its validity
+            $from = $getBy($collection, 'getId', $id);
+            $order = $from->$get();
+            if (($order >= count($collection) && $dir == 'down') || ($order <= 1 && $dir == 'up')) {
+                return $collection;
+            }
+
+            if ($dir == 'up') $order--;
+            elseif ($dir == 'down') $order++;
+
+            //switch the orders
+            $to = $getBy($collection, $get, $order);
+            $to->$set($from->$get());
+            $from->$set($order);
+        }
+
+        return $collection;
+    }
 }

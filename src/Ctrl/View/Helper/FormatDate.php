484a5f81d9959a8686d94afc3b8b28bd71678c9a
ctrl-f5 <mythix.dev@gmail.com>
2012-08-12 22:11:11 +0200

    added date and time select option

--- src/Ctrl/View/Helper/FormatDate.php
+++ src/Ctrl/View/Helper/FormatDate.php
@@ -6,14 +6,19 @@ use Ctrl\View\Helper\AbstractHtmlElement;
 
 class FormatDate extends AbstractHtmlElement
 {
-    protected $defaultFormat = 'Y-m-d H:i';
+    protected $defaultFormat        = 'Y-m-d H:i';
+    protected $defaultDateFormat    = 'Y-m-d';
+    protected $defaultTimeFormat    = 'H:i';
 
     public function __invoke($date, $format = null)
     {
         if (!($date instanceof \DateTime))
             return '';
 
+        if ($format == 'date') $format = $this->defaultDateFormat;
+        if ($format == 'time') $format = $this->defaultTimeFormat;
         if (!$format) $format = $this->defaultFormat;
+
         $html = $this->create($date, $format);
 
         return $html;

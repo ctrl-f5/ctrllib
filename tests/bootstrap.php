cd66fb2c84779b258f49d28b4aceb594732d8144
ctrl-f5 <mythix.dev@gmail.com>
2012-11-15 01:35:15 +0100

    added bootstrap error message for phpunit

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,8 +1,11 @@
 <?php
 
 error_reporting(E_STRICT);
+ini_set('display_errors', 1);
 
 $autoload = __DIR__.'/../vendor/autoload.php';
 if (file_exists($autoload)) {
     $loader = include $autoload;
-}
+} else {
+    die('could not find autoload in:'.$autoload.PHP_EOL);
+} 
